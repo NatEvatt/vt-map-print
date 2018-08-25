8f0f06a34dfec74cddb726e77e08db885fcc60f4
Nat Evatt <natevatt@gmail.com>
2018-08-25 14:46:54 -0700

    bugfix: fixed pixel count in options

--- vt_map_print/printer.py
+++ vt_map_print/printer.py
@@ -107,7 +107,7 @@ class VT_Map_Print():
         parser.add_argument("bottom_right_lat", help="the latitudinal position of the bottom right point in your bbox", type=float)
         parser.add_argument("bottom_right_lon", help="the longitudinal position of the bottom right point in your bbox", type=float)
         parser.add_argument("-a", "--api_token", help="override the config api_token with another Mapbox API Token")
-        parser.add_argument("-p", "--pixels", help="number of pixels in tile - accepts '256' or '512'")
+        parser.add_argument("-p", "--pixels", help="number of pixels in tile - accepts '256' or '512'", type=int)
         parser.add_argument("-r", "--retina", help="accepts 'Y' or 'N' default is N")
         parser.add_argument("-s", "--style_id", help="specify the Mabox Style ID default is streets")
         parser.add_argument("-u", "--mapbox_url", help="override the config mapbox_url with another Mapbox url")
